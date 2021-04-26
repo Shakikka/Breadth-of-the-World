87f3730adb6f57483c892e20fbb19b0486a4d204
Joel <wilddroppings@gmail.com>
2021-04-26 00:51:47 -0600

    Add conditional rendering

--- src/Item/Item.js
+++ src/Item/Item.js
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 
 const Item = ({ category, common_locations, cooking_effect, description, hearts_recovered, image, name, drops, attack, defense,  }) => {
+        console.log('hearts', hearts_recovered)
     return (
         <div>
             <Link to={`/${category}`}>
@@ -10,12 +11,10 @@ const Item = ({ category, common_locations, cooking_effect, description, hearts_
             <p>{name}</p>
             <p>{description}</p>
             <p>Can be found at: {common_locations}</p>
-            <p>Attack: {attack}</p>
-            <p>Defense: {defense}</p>
-            <p>Drops: {drops}</p>
-            <p>Cooking Effects: {cooking_effect}</p>
-            <p>Hearts Restored: {hearts_recovered}</p>
-
+            {category === 'equipment' && <div><p>Attack: {attack}</p><p>Defense: {defense}</p></div>}
+            {drops && <p>Drops: {drops}</p>}
+            {cooking_effect && <p>Cooking Effects: {cooking_effect}</p>}
+            {hearts_recovered !== 0 && hearts_recovered && <p>Hearts Restored: {hearts_recovered}</p>}
         </div>
     )
 }
