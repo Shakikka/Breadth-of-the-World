1f9275fe479337f3ae5b65ed0fa4eb46b70cf5d5
Joel <wilddroppings@gmail.com>
2021-04-25 23:43:09 -0600

    Deleted old routes

--- src/App/App.js
+++ src/App/App.js
@@ -48,13 +48,8 @@ class App extends Component {
       <div className="App">
         <Switch>
           <Route exact path='/' render={() => <HomePage/>}/>
-          {/* <Route exact path='/creatures' render={() => <Creatures food={this.state.creatures.food} nonFood={this.state.creatures.nonFood}/>} />
-          <Route exact path= '/equipment' render={() => <Equipment equipment={this.state.equipment}/>} />
-          <Route exact path='/materials' render={() => <Materials materials={this.state.materials}/>} />
-          <Route exact path='/monsters' render={() => <Monsters monsters={this.state.monsters}/>} />
-          <Route exact path='/treasure' render={() => <Treasure treasure={this.state.treasure}/>} /> */}
           <Route exact path='/:category' render={({ match }) => {
-            const { name, category } = match.params;
+            const { category } = match.params;
             return <Items data={this.listItems(category)}/>}
           } />
         </Switch>
