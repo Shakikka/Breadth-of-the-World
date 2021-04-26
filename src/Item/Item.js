6ba371a8c791ccf86e82e24f8ab8a729658b6600
Joel <wilddroppings@gmail.com>
2021-04-26 16:58:08 -0600

    Join drops and location so they are not smooshed

--- src/Item/Item.js
+++ src/Item/Item.js
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom';
 
 const Item = ({ category, common_locations, cooking_effect, description, hearts_recovered, image, name, drops, attack, defense, clearFoundItems }) => {
-    console.log(image)
+ 
     return (
         <div>
             <Link to={`/${category}`}>
@@ -13,10 +13,10 @@ const Item = ({ category, common_locations, cooking_effect, description, hearts_
             <img src={image} alt=''/>
             <p>{name}</p>
             <p>{description}</p>
-            {common_locations && <p>Can be found at: {common_locations}</p>}
+            {common_locations && <p>Can be found at: {common_locations.join(', ')}</p>}
             {attack !== 0 && attack && <p>Attack: {attack}</p>}
             {defense !== 0 && defense && <p>Defense: {defense}</p>}
-            {drops && <p>Drops: {drops}</p>}
+            {drops && <p>Drops: {drops.join(', ')}</p>}
             {cooking_effect && <p>Cooking Effects: {cooking_effect}</p>}
             {hearts_recovered !== 0 && hearts_recovered && <p>Hearts Restored: {hearts_recovered}</p>}
         </div>
