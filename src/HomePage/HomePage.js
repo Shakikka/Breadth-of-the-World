17652cd5d0a12ca54f7c60efe021b7c0eac57f8f
Joel <wilddroppings@gmail.com>
2021-04-25 18:56:31 -0600

    Setup basic HomePage component

---
+++ src/HomePage/HomePage.js
@@ -0,0 +1,21 @@
+import './HomePage.css';
+import React, { Component } from 'react';
+
+class HomePage extends Component {
+    constructor() {
+        super()
+        this.state = {
+            creatures: [],
+            equipment: [],
+            materials: [],
+            monsters: [],
+            treasure: []
+        }
+    }
+
+    componentDidMount() {
+        
+    }
+}
+
+export default HomePage;
