2d482ffa7538e47505d46c9edf1e25d3a198feda
Joel <wilddroppings@gmail.com>
2021-04-25 22:10:39 -0600

    Add links for buttons

--- src/App/App.js
+++ src/App/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import { Link, Route, Switch } from 'react-router-dom';
+import HomePage from '../HomePage/HomePage';
 
 const App = () => {
   return (
     <div className="App">
-      
+      <HomePage/>
     </div>
   );
 }
