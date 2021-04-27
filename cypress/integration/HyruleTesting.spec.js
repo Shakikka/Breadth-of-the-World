3acd1d00877a4a07e31bdc17eba54cc80b8bf403
Joel <wilddroppings@gmail.com>
2021-04-26 18:58:12 -0600

    Add beforeEach statement with get stub

--- cypress/integration/HyruleTesting.spec.js
+++ cypress/integration/HyruleTesting.spec.js
@@ -1,3 +1,11 @@
 describe('Breadth of the World homepage functionality', () => {
-    
+
+    beforeEach(() => {
+        cy.fixture('get-items-data.json').then((itemInfo => {
+            cy.intercept('https://botw-compendium.herokuapp.com/api/v2', itemInfo)
+        }))
+        cy.visit('http://localhost:3000')
+    })
+
+    it
 })
